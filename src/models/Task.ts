f29a851b01eb40d1f7604c26685a5089e93f1042
BosyCode <hubert.kliszcz@gmail.com>
2025-03-16 22:02:09 +0100

    Add CRUD API for tasks

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const TaskSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String, required: false },
   completed: { type: Boolean, default: false },
 }, { timestamps: true });
 
