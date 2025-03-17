fa4cf69f280a2747d738f7a68beb6c16be9a5278
BosyCode <hubert.kliszcz@gmail.com>
2025-03-18 00:46:53 +0100

    add user model

---
+++ src/models/User.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema: Schema<IUser> = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+}, { timestamps: true });
+
+export default mongoose.model<IUser>("User", UserSchema);
